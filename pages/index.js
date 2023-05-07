37ca2c9872b629c9327f9b87350586f52afa43c8
Mohnish Sharma <mohnishsharma78@gmail.com>
2023-05-07 14:39:17 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -25,12 +25,12 @@ export default function Home() {
 
   return (
     <div className="flex flex-col items-center relative min-h-screen">
-      <h2 className="font-raleway font-bold text-6xl text-primary pt-20 pb-6 md:text-3xl">
-        Email <span className="text-secondary">Verification</span> by Verification Saga
+      <h2 className="font-raleway font-bold text-6xl text-primary pt-20 pb-20 md:text-4xl">
+        Email <span className="text-secondary">Validation</span> by Verification Saga
       </h2>
-      <h3 className="text-lightGrey text-2xl font-raleway font-bold uppercase tracking-wide mb-12 md:text-base md:px-4 md:text-center">
-        Check if an email address exists or not
-      </h3>
+     {/*  <h3 className="text-lightGrey text-2xl font-raleway font-bold uppercase tracking-wide mb-12 md:text-base md:px-4 md:text-center">
+      
+      </h3> */}
       <div className="flex flex-col justify-between items-center w-full md:items-center">
         <form
           className="flex w-full justify-center md:flex-col md:w-5/6"
